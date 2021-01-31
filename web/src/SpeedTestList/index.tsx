a180a48d37450ff3b947e886fa8117264e7c6cb2
Janderson Martins <jandersonmartins10@gmail.com>
2021-01-31 18:02:08 -0300

    feature(web): load more button

--- web/src/SpeedTestList/index.tsx
+++ web/src/SpeedTestList/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import { useSpeedTestData } from './useSpeedTestData'
 import SpeedTestData from './SpeedTestData'
@@ -6,40 +6,60 @@ import SpeedTestData from './SpeedTestData'
 const BASE_URL = process.env.REACT_APP_SCREENSHOT_BASE_URL ?? 'http://localhost:3001'
 
 const SpeedTestList = () => {
-  const { data } = useSpeedTestData()
+  const {
+    data,
+    loading,
+    error,
+    nextPage
+  } = useSpeedTestData()
+
+  const handleOnLoadMoreClick = useCallback(nextPage, [nextPage])
 
   return (
     <div>
       <h1>SpeedTestList</h1>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Download</th>
-            <th>Upload</th>
-            <th>Ping</th>
-            <th>Date/Time</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((d: SpeedTestData) =>
-            <tr key={d._id}>
-              <td>{d.downloadSpeed} {d.downloadUnit}</td>
-              <td>{d.uploadSpeed} {d.uploadUnit}</td>
-              <td>{d.ping} {d.pingUnit}</td>
-              <td>{d.dateTime}</td>
-              <td>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={`${BASE_URL}/files?name=${d.screenshot}`}
-                >Screenshot</a>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      {error ? (
+        <div>
+          <p>{error}</p>
+        </div>
+      ) : (
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>Download</th>
+                <th>Upload</th>
+                <th>Ping</th>
+                <th>Date/Time</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((d: SpeedTestData) =>
+                <tr key={d._id}>
+                  <td>{d.downloadSpeed} {d.downloadUnit}</td>
+                  <td>{d.uploadSpeed} {d.uploadUnit}</td>
+                  <td>{d.ping} {d.pingUnit}</td>
+                  <td>{d.dateTime}</td>
+                  <td>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={`${BASE_URL}/files?name=${d.screenshot}`}
+                    >Screenshot</a>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+
+          <button
+            disabled={loading}
+            onClick={handleOnLoadMoreClick}
+          >Load More</button>
+        </>
+      )}
     </div>
   )
 }
--- web/src/SpeedTestList/useSpeedTestData.ts
+++ web/src/SpeedTestList/useSpeedTestData.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import SpeedTestData from './SpeedTestData'
 
@@ -6,32 +6,49 @@ const SERVER_URL = process.env.REACT_APP_SERVER_URL ?? 'http://localhost:4001'
 
 export interface UseSpeedTestOutput {
   data: SpeedTestData[]
+  loading: boolean
+  error: string | null
   nextPage: () => void
+}
+
+type IdMap = {
+  [key: string]: boolean
 }
 
 const useSpeedTestData = (): UseSpeedTestOutput => {
   const [page, setPage] = useState<number>(1)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
   const [data, setSpeedTestData] = useState<SpeedTestData[]>([])
 
-  useEffect(() => {
-    const load = async (pageRequest: number): Promise<void> => {
-      try {
-        const res = await fetch(`${SERVER_URL}/speed-tests?page=${pageRequest}`)
-        const jsonData: SpeedTestData[] = await res.json()
-        if (jsonData.length) {
-          setSpeedTestData(d => [...d, ...jsonData])
-        }
-      } catch (err) {
-        // TODO: return error
+  const load = useCallback(async (pageRequest: number = 1): Promise<void> => {
+    setLoading(true)
+    setError(null)
+    try {
+      const res = await fetch(`${SERVER_URL}/speed-tests?page=${pageRequest}`)
+      const jsonData: SpeedTestData[] = await res.json()
+      if (jsonData.length) {
+        setSpeedTestData(currentData => {
+          const ids: IdMap = currentData.reduce((ids, d) => ({ ...ids, [d._id]: true }), {})
+          const newData = jsonData.filter(d => !ids[d._id])
+          return [...currentData, ...newData]
+        })
+        setPage(p => p + 1)
       }
+    } catch (err) {
+      setError(err.message)
+    } finally {
+      setLoading(false)
     }
+  }, [])
 
-    load(page)
-  }, [page])
+  useEffect(() => {
+    load()
+  }, [load])
 
-  const nextPage = () => setPage(page + 1)
+  const nextPage = useCallback(() => load(page), [page, load])
 
-  return { data, nextPage }
+  return { data, loading, error, nextPage }
 }
 
 export { useSpeedTestData }
